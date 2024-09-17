8ec886257417499403cacc32e58a3a9a8908771e
felgaceltica <rodrigo.felga@celtica.com.br>
2024-09-17 19:36:58 -0300

    Merge remote-tracking branch 'sunflower-land/main'

--- src/lib/utils/fonts.ts
+++ src/lib/utils/fonts.ts
@@ -28,7 +28,7 @@ export const FONT_CONFIG: Record<Font, FontSettings> = {
   Default: {
     fontFamily: "Basic",
     xxs: [20, 14],
-    xs: [24, 14],
+    xs: [24, 16],
     sm: [30, 20],
     base: [36, 26],
     lg: [42, 32],
@@ -62,11 +62,11 @@ export const FONT_CONFIG: Record<Font, FontSettings> = {
 export const CYRILLIC_FONT_CONFIG: Record<Font, FontSettings> = {
   Default: {
     fontFamily: "Basis33",
-    xxs: [20, 14],
-    xs: [24, 14],
-    sm: [30, 20],
-    base: [36, 26],
-    lg: [42, 32],
+    xxs: [18, 14],
+    xs: [20, 16],
+    sm: [26, 22],
+    base: [32, 26],
+    lg: [40, 32],
   },
   "Sans Serif": {
     fontFamily: "sans-serif",
@@ -78,14 +78,34 @@ export const CYRILLIC_FONT_CONFIG: Record<Font, FontSettings> = {
   },
   Bold: {
     fontFamily: "Born2bSporty",
-    xxs: [16, 12],
-    xs: [20, 14],
-    sm: [26, 20],
+    xxs: [14, 12],
+    xs: [18, 16],
+    sm: [22, 20],
     base: [30, 26],
-    lg: [36, 32],
+    lg: [34, 30],
   },
   "Chunky (Old)": {
     fontFamily: "Russo One",
+    xxs: [14, 14],
+    xs: [16, 16],
+    sm: [18, 18],
+    base: [24, 24],
+    lg: [28, 28],
+  },
+};
+
+export const CHINESE_FONT_CONFIG: Partial<Record<Font, FontSettings>> = {
+  Default: {
+    // Pixel
+    fontFamily: "Ark",
+    xxs: [14, 14],
+    xs: [16, 16],
+    sm: [18, 18],
+    base: [25, 25],
+    lg: [30, 30],
+  },
+  "Sans Serif": {
+    fontFamily: "sans-serif",
     xxs: [14, 14],
     xs: [16, 16],
     sm: [18, 18],
@@ -94,10 +114,9 @@ export const CYRILLIC_FONT_CONFIG: Record<Font, FontSettings> = {
   },
 };
 
-export const CHINESE_FONT_CONFIG: Partial<Record<Font, FontSettings>> = {
+export const KOREAN_FONT_CONFIG: Partial<Record<Font, FontSettings>> = {
   Default: {
-    // Pixel
-    fontFamily: "Ark",
+    fontFamily: "Noto Serif KR",
     xxs: [14, 14],
     xs: [16, 16],
     sm: [18, 18],
@@ -123,57 +142,64 @@ export function initialiseFont() {
 function setFontProperties(config: FontSettings) {
   document.documentElement.style.setProperty(
     "--font-family",
-    config.fontFamily
+    config.fontFamily,
   );
   document.documentElement.style.setProperty(
     "--text-xxs-size",
-    `${config.xxs[0]}px`
+    `${config.xxs[0]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-xxs-line-height",
-    `${config.xxs[1]}px`
+    `${config.xxs[1]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-xs-size",
-    `${config.xs[0]}px`
+    `${config.xs[0]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-xs-line-height",
-    `${config.xs[1]}px`
+    `${config.xs[1]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-sm-size",
-    `${config.sm[0]}px`
+    `${config.sm[0]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-sm-line-height",
-    `${config.sm[1]}px`
+    `${config.sm[1]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-base-size",
-    `${config.base[0]}px`
+    `${config.base[0]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-base-line-height",
-    `${config.base[1]}px`
+    `${config.base[1]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-lg-size",
-    `${config.lg[0]}px`
+    `${config.lg[0]}px`,
   );
   document.documentElement.style.setProperty(
     "--text-lg-line-height",
-    `${config.lg[1]}px`
+    `${config.lg[1]}px`,
   );
 }
 
 export function changeFont(font: Font) {
   const lang = localStorage.getItem("language") || "en";
-  const config = ["ru"].includes(lang)
-    ? CYRILLIC_FONT_CONFIG[font]
-    : ["zh-CN"].includes(lang)
-    ? CHINESE_FONT_CONFIG[font]
-    : FONT_CONFIG[font];
+  const config = (() => {
+    switch (lang) {
+      case "ru":
+        return CYRILLIC_FONT_CONFIG[font];
+      case "zh-CN":
+        return CHINESE_FONT_CONFIG[font];
+      case "ko":
+        return KOREAN_FONT_CONFIG[font];
+      default:
+        return FONT_CONFIG[font];
+    }
+  })();
 
   if (config) {
     setFontProperties(config);
