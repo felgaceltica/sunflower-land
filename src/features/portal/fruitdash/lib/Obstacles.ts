e4c4b1871bb2314bbada3e2a1ba425a64cf932fb
felgaceltica <rodrigo.felga@celtica.com.br>
2024-09-23 20:30:12 -0300

    halloween adjustment

--- src/features/portal/fruitdash/lib/Obstacles.ts
+++ src/features/portal/fruitdash/lib/Obstacles.ts
@@ -39,7 +39,7 @@ export class FruitDashObstacleFactory {
 
     if (this.IS_HALLOWEEN) {
       this.obstacles["oilpit"] = new OilPitHalloweenObstacle(20, 5, "obstacle");
-      this.obstacles["rock"] = new RockHalloweenObstacle(100, 2, "obstacle");
+      this.obstacles["rock"] = new RockObstacle(100, 2, "obstacle");
       this.obstacles["largerock"] = new StoneRockHalloweenObstacle(
         20,
         5,
@@ -51,14 +51,17 @@ export class FruitDashObstacleFactory {
         "obstacle",
       );
       this.obstacles["gravestone"] = new GraveStoneObstacle(50, 2, "obstacle");
+      //Points
+      this.obstacles["fruit"] = new FruitHalloweenObstacle(50, 20, "bounty");
     } else {
       this.obstacles["oilpit"] = new OilPitObstacle(20, 5, "obstacle");
       this.obstacles["rock"] = new RockObstacle(100, 2, "obstacle");
       this.obstacles["largerock"] = new StoneRockObstacle(20, 5, "obstacle");
       this.obstacles["gravestone"] = new GraveStoneObstacle(100, 2, "obstacle");
+      //Points
+      this.obstacles["fruit"] = new FruitObstacle(50, 20, "bounty");
     }
     //Points
-    this.obstacles["fruit"] = new FruitObstacle(50, 20, "bounty");
     this.obstacles["bounty"] = new ChestObstacle(0, 250, "bounty");
     //PowerUps
     this.obstacles["slowdown"] = new SlowDownPowerUp(0, 0, "powerup");
@@ -1203,6 +1206,43 @@ class CoinObstacle extends FruitDashObstacle {
     return container;
   }
 }
+
+class FruitHalloweenObstacle extends FruitDashObstacle {
+  add(scene: FruitDashBaseScene, name: string): FruitDashObstacleContainer {
+    const container = new FruitDashObstacleContainer(
+      scene,
+      0,
+      START_HEIGHT - SQUARE_WIDTH_TEXTURE * 2,
+      this._weight,
+      this._points,
+      this._type,
+      "Pumpkim",
+    );
+    const image = scene.add.sprite(0, 0, "SunnySideSpritesHalloween", 25);
+    image.setOrigin(0, 0);
+    container.add(image);
+    image.play("pumpkim");
+    const bounds = container.getBounds();
+    const shape = new Phaser.Geom.Circle(
+      SQUARE_WIDTH_TEXTURE / 2 - 1,
+      SQUARE_WIDTH_TEXTURE / 2,
+      SQUARE_WIDTH_TEXTURE / 2,
+    );
+    shape.radius = shape.radius * 0.8;
+    if (scene.physics.world.drawDebug) {
+      const graphics = new Phaser.GameObjects.Graphics(scene, {
+        lineStyle: { width: 1, color: 0xffff00 },
+        fillStyle: { color: 0xff0000 },
+      });
+      graphics.strokeCircleShape(shape);
+      graphics.setDepth(1000);
+      container.add(graphics);
+    }
+    container.setCollisionCircle(shape);
+    return container;
+  }
+}
+
 class FruitObstacle extends FruitDashObstacle {
   add(scene: FruitDashBaseScene, name: string): FruitDashObstacleContainer {
     const fruits = ["Apple", "Banana", "Orange", "Blueberry"];
