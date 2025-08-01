648f851a7f80ad6b18451a38d3e90a2f2aabca00
felgaceltica <rodrigo.felga@celtica.com.br>
2025-08-01 09:37:23 -0300

    Merge remote-tracking branch 'sunflower-land/main'

--- src/features/island/collectibles/components/JanitorChicken.tsx
+++ src/features/island/collectibles/components/JanitorChicken.tsx
@@ -5,21 +5,21 @@ import { ITEM_DETAILS } from "features/game/types/images";
 import { ImageStyle } from "./template/ImageStyle";
 import { SFTDetailPopover } from "components/ui/SFTDetailPopover";
 
-export const LoveChicken: React.FC<CollectibleProps> = () => {
+export const JanitorChicken: React.FC<CollectibleProps> = () => {
   return (
-    <SFTDetailPopover name="Love Chicken">
+    <SFTDetailPopover name="Janitor Chicken">
       <ImageStyle
-        name="Love Chicken"
+        name="Janitor Chicken"
         divStyle={{
-          width: `${PIXEL_SCALE * 21}px`,
+          width: `${PIXEL_SCALE * 26}px`,
           bottom: `${PIXEL_SCALE * 0}px`,
           left: `${PIXEL_SCALE * -1.75}px`,
         }}
         imgStyle={{
-          width: `${PIXEL_SCALE * 21}px`,
+          width: `${PIXEL_SCALE * 26}px`,
         }}
-        image={ITEM_DETAILS["Love Chicken"].image}
-        alt="Love Chicken"
+        image={ITEM_DETAILS["Janitor Chicken"].image}
+        alt="Janitor Chicken"
       />
     </SFTDetailPopover>
   );
