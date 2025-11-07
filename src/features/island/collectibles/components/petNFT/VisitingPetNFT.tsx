088060d9b6817b5d9d4e916296e3ba1c308655c5
felgaceltica <rodrigo.felga@celtica.com.br>
2025-11-06 21:45:23 -0300

    Merge remote-tracking branch 'sunflower-land/main' (parte 4)

---
+++ src/features/island/collectibles/components/petNFT/VisitingPetNFT.tsx
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import { useSelector } from "@xstate/react";
+import {
+  isPetNeglected,
+  isPetNapping,
+  isPetOfTypeFed,
+  hasHitSocialPetLimit,
+} from "features/game/types/pets";
+import { PetSprite } from "features/island/pets/PetSprite";
+import { useContext } from "react";
+import { Context } from "features/game/GameProvider";
+import { MachineState } from "features/game/lib/gameMachine";
+import { SUNNYSIDE } from "assets/sunnyside";
+import { Modal } from "components/ui/Modal";
+import { CloseButtonPanel } from "features/game/components/CloseablePanel";
+import { PIXEL_SCALE } from "features/game/lib/constants";
+import { FarmHelped } from "features/island/hud/components/FarmHelped";
+import { hasFeatureAccess } from "lib/flags";
+import { isHelpComplete } from "features/game/types/monuments";
+
+const _petNFTData = (id: string) => (state: MachineState) => {
+  return state.context.state.pets?.nfts?.[Number(id)];
+};
+
+const _isTypeFed = (id: string) => (state: MachineState) => {
+  const petData = state.context.state.pets?.nfts?.[Number(id)];
+  if (!petData) return false;
+  if (!petData.traits) return false;
+
+  const isTypeFed = isPetOfTypeFed({
+    nftPets: state.context.state.pets?.nfts ?? {},
+    petType: petData.traits.type,
+    id: Number(id),
+    now: Date.now(),
+  });
+
+  return isTypeFed;
+};
+
+const _hasHelpedPet = (id: number) => (state: MachineState) => {
+  if (state.context.visitorState) {
+    const hasAccess = hasFeatureAccess(state.context.visitorState, "PETS");
+    if (!hasAccess) {
+      return true;
+    }
+
+    const hasHelpedToday = state.context.hasHelpedPlayerToday ?? false;
+
+    const hasHelpedPet = !!state.context.state.pets?.nfts?.[id]?.visitedAt;
+
+    return hasHelpedPet || hasHelpedToday;
+  }
+};
+
+export const VisitingPetNFT: React.FC<{
+  id: string;
+}> = ({ id }) => {
+  const { gameService } = useContext(Context);
+  const petNFTData = useSelector(gameService, _petNFTData(id));
+  const isTypeFed = useSelector(gameService, _isTypeFed(id));
+  const hasHelpedPet = useSelector(gameService, _hasHelpedPet(Number(id)));
+  const [showHelped, setShowHelped] = useState(false);
+
+  const visitorGameState = useSelector(
+    gameService,
+    (state) => state.context.visitorState,
+  );
+  const state = useSelector(gameService, (state) => state.context.state);
+  const totalHelpedToday = useSelector(
+    gameService,
+    (state) => state.context.totalHelpedToday ?? 0,
+  );
+
+  if (!petNFTData || !petNFTData.traits) return null;
+
+  const isNeglected = isPetNeglected(petNFTData);
+  const isNapping = isPetNapping(petNFTData);
+
+  const handlePetClick = () => {
+    if (
+      petNFTData &&
+      visitorGameState &&
+      hasFeatureAccess(visitorGameState, "PETS") &&
+      !hasHelpedPet
+    ) {
+      gameService.send("pet.visitingPets", {
+        pet: Number(id),
+        totalHelpedToday,
+      });
+
+      if (
+        isHelpComplete({
+          game: gameService.getSnapshot().context.state,
+        })
+      ) {
+        setShowHelped(true);
+      }
+    }
+  };
+
+  return (
+    <PetSprite
+      id={Number(id)}
+      isNeglected={isNeglected}
+      isNapping={isNapping}
+      isTypeFed={isTypeFed}
+      onClick={handlePetClick}
+      clickable={!hasHelpedPet}
+    >
+      {!hasHelpedPet && petNFTData && !hasHitSocialPetLimit(petNFTData) && (
+        <div
+          className="absolute -top-4 -right-4 pointer-events-auto cursor-pointer hover:img-highlight"
+          onClick={(e) => {
+            e.stopPropagation();
+            handlePetClick();
+          }}
+        >
+          <div
+            className="relative mr-2"
+            style={{
+              width: `${PIXEL_SCALE * 20}px`,
+              top: `${PIXEL_SCALE * -4}px`,
+              right: `${PIXEL_SCALE * -4}px`,
+            }}
+          >
+            <img className="w-full" src={SUNNYSIDE.icons.disc} />
+            <img
+              className="absolute"
+              src={SUNNYSIDE.icons.drag}
+              style={{
+                width: `${PIXEL_SCALE * 14}px`,
+                right: `${PIXEL_SCALE * 3}px`,
+                top: `${PIXEL_SCALE * 2}px`,
+                zIndex: 1000,
+              }}
+            />
+          </div>
+        </div>
+      )}
+      <Modal show={showHelped}>
+        <CloseButtonPanel bumpkinParts={state.bumpkin.equipped}>
+          <FarmHelped onClose={() => setShowHelped(false)} />
+        </CloseButtonPanel>
+      </Modal>
+    </PetSprite>
+  );
+};
